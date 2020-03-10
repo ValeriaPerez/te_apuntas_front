62c4d2653d2c81067f29a52ecb336707e3007860
AMADEUXX <jta2902@hotmail.com>
2020-03-10 00:08:05 -0600

    Add slidercomponent for welcome page

--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -4,12 +4,39 @@ import React, { Component } from "react";
 import Header from '../components/Header';
 import Modal from '../components/Modal';
 import Destination from '../components/Destination';
+import Slider from '../components/SliderImage';
 
 export default class HomeLogged extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      height: 0,
+      width: 0,
+      showBullets: false
+    };
+    window.addEventListener("resize", this.update);
+  }
+
+  componentDidMount() {
+    this.update();
+  }
+
+  update = () => {
+    this.setState({
+      height: window.innerHeight,
+      width: window.innerWidth
+    });
+  };
+
   render() {
     return (
       <div className='Home'>
         <div className='BG-img'>
+          <Slider
+            width={this.state.width}
+            height={this.state.height}
+            showBullets={this.state.showBullets} />
           <Header />
           <section className='Body'>
             <Modal />
