7f3986056194700844b157154e2531f7fffb6d44
kike.durang <jodurang@aeromexico.com>
2020-03-10 23:16:03 -0600

    Se agrego slider en Home

--- src/components/SliderImage.js
+++ src/components/SliderImage.js
@@ -14,22 +14,22 @@ class Slider extends React.Component {
 
    render() {
      console.log(this.props.width);
-     const images = [
-       { url: "images/1.jpg" },
-       { url: "images/2.jpg" },
-       { url: "images/3.jpg" },
-       { url: "images/4.jpg" },
-       { url: "images/5.jpg" },
-       { url: "images/6.jpg" },
-       { url: "images/7.jpg" },
-     ];
+    //  const images = [
+    //    { url: "images/1.jpg" },
+    //    { url: "images/2.jpg" },
+    //    { url: "images/3.jpg" },
+    //    { url: "images/4.jpg" },
+    //    { url: "images/5.jpg" },
+    //    { url: "images/6.jpg" },
+    //    { url: "images/7.jpg" },
+    //  ];
 
      return (
        <div>
          <SimpleImageSlider
            width={this.props.width}
            height={this.props.height}
-           images={images}
+           images={this.props.images}
            showBullets={this.props.showBullets}
          />
        </div>
