6b61ca7d77454af7bbb5d0878c2bc267d818b986
ValeriaPerez <valeria.perez@rappi.com>
2020-03-24 21:36:02 -0600

    Add loader

---
+++ src/components/elements/loader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Loader(props) {
+  return (
+    <div className='loader-container'>
+
+      <div className='loader'>
+        <span></span>
+        <span></span>
+      </div>
+
+    </div>
+  );
+}
+
+export default Loader
